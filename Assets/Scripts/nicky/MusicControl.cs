d0f4b6e4e6620f11a8f1ed04e8acba275d1ab1b5
nick199646 <161616114+nick199646@users.noreply.github.com>
2026-08-30 17:23:45 +0800

    yinxiao

--- Assets/Scripts/nicky/MusicControl.cs
+++ Assets/Scripts/nicky/MusicControl.cs
@@ -8,9 +8,17 @@ public class MusicControl : MonoBehaviour
     {
         AudioManager.Instance.PlaySfx("ClickUI",1f);
     }
+    public void UIClick2()
+    {
+        AudioManager.Instance.PlaySfx("UIClick", 1f);
+    }
     public void PlayChapterBgm()
     {
         AudioManager.Instance.PlayMusic("Chapter",1f);
 
+    }
+    public void PlayShuyeSFX()
+    {
+        AudioManager.Instance.PlaySfx("shuye", 1f);
     }
 }
